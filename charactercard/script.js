3752aaed163f7c9c2ed3efba4b8fc7aa64646789
GodwinOriamhen01 <ori23003@byui.edu>
2026-07-06 03:25:02 -0600

    doneanddone

--- charactercard/script.js
+++ charactercard/script.js
@@ -2,7 +2,7 @@ const character = {
     name: "Snortleblat",
     class: "Swamp Beast Diplomat",
     level: 8,
-    health: 100,
+    health: 0,
     image: "images/snortleblat.png",
 
     attacked() {
